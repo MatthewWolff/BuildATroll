3bd92ca5439f87c9efaba47fee4cef6e78842935
Matthew Wolff <mm.wolff@chi-squared.org>
2018-04-21 03:21:28 -0500

    connected bot corpus to markovify

--- markov.py
+++ markov.py
@@ -12,7 +12,7 @@ import pandas as pd
 def get_model(corpus):
     file_id = corpus.split("_")[0]
     model_path = file_id + "_markov_model.json"
-    if not path.exists(model_path):  # if model exists, use it
+    if not path.exists(model_path):  # if model doesn't exist, make it
         # Get raw text as string.
         with open(corpus) as f:
             text = f.read()
@@ -36,36 +36,43 @@ def clean(string):
 
 
 def get_corpus():
-    # russian trolls, with an ID set for constant time user access
-    users = pd.read_csv("users.csv")
-    users.set_index(keys="id", inplace=True, drop=False)
-    tweets = pd.read_csv("tweets.csv")
+    bot_corpus = "bot_corpus.txt"
+    if not path.exists(bot_corpus):  # if corpus doesn't exist, make it
+        # russian trolls, with an ID set for constant time user access
+        users = pd.read_csv("users.csv")
+        users.set_index(keys="id", inplace=True, drop=False)
+        tweets = pd.read_csv("tweets.csv")
 
-    # sort users by their ratio of favorites per follower
-    users["favorites_per_follower"] = users.apply(lambda row: row.favourites_count / (row.followers_count + 1), axis=1)
-    users.sort_values(by=["favorites_per_follower"], ascending=False, inplace=True)
+        # sort users by their ratio of favorites per follower
+        users["favorites_per_follower"] = users.apply(lambda row: row.favourites_count / (row.followers_count + 1),
+                                                      axis=1)
+        users.sort_values(by=["favorites_per_follower"], ascending=False, inplace=True)
 
-    # create a column of empty lists to collect tweets in, then make a list of dictionaries from users
-    users["tweets"] = np.empty((len(users), 0)).tolist()
-    curated_tweets = pd.Series(users.tweets.values, index=users.id).to_dict()
+        # create a column of empty lists to collect tweets in, then make a list of dictionaries from users
+        users["tweets"] = np.empty((len(users), 0)).tolist()
+        curated_tweets = pd.Series(users.tweets.values, index=users.id).to_dict()
 
-    for index, tweet in tweets.iterrows():
-        if isnan(tweet.user_id) or "RT" in str(tweet.text):  # ignore unidentified tweets and retweets 
-            continue
-        # curate list of tweets and their # of favorites
-        curated_tweets[tweet.user_id].append([str(tweet.text), tweet.favorite_count if not "nan" else 0])
-    # sort the list by # of favorites then combine its contents into a corpus, taking care to clean the text
-    corpus = " ".join(" ".join(clean(z[0]) for z in sorted(x, key=lambda y: y[1])) for x in curated_tweets.values())
-    return corpus
+        for index, tweet in tweets.iterrows():
+            if isnan(tweet.user_id) or "RT" in str(tweet.text):  # ignore unidentified tweets and re-tweets
+                continue
+            # curate list of tweets and their # of favorites
+            curated_tweets[tweet.user_id].append([str(tweet.text), tweet.favorite_count if not "nan" else 0])
+        # sort the list by # of favorites then combine its contents into a corpus, taking care to clean the text
+        corpus = " ".join(" ".join(clean(z[0]) for z in sorted(x, key=lambda y: y[1])) for x in curated_tweets.values())
+        with open(bot_corpus, "wb") as o:
+            o.write(corpus)
+    else:
+        # Get raw text as string.
+        with open(bot_corpus) as f:
+            corpus = f.read()
+    return bot_corpus  # lol return the file name for now
 
 
 if __name__ == "__main__":
-    get_corpus()
 
-    # # Print five randomly-generated sentences
     # corpus_name = "realdonaldtrump_readable_corpus.txt"
-    # markov_model = get_model(corpus_name)
-    #
-    # # Print three randomly-generated sentences of no more than 140 characters
-    # for i in range(3):
-    #     print(markov_model.make_short_sentence(140))
+    corpus_name = get_corpus()
+    markov_model = get_model(corpus_name)
+
+    for i in range(10):
+        print "The Russians say:", markov_model.make_short_sentence(280)  # characters
