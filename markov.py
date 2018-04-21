e451f2550d6e42c8f18d8ef88ac31ee1da033cce
Matthew Wolff <mm.wolff@chi-squared.org>
2018-04-21 03:05:14 -0500

    imported and cleaned CSV/generated corpus

--- markov.py
+++ markov.py
@@ -1,5 +1,7 @@
 #!/bin/python
 import json
+import re
+from math import isnan
 from os import path
 
 import markovify
@@ -10,7 +12,7 @@ import pandas as pd
 def get_model(corpus):
     file_id = corpus.split("_")[0]
     model_path = file_id + "_markov_model.json"
-    if not path.exists(model_path):
+    if not path.exists(model_path):  # if model exists, use it
         # Get raw text as string.
         with open(corpus) as f:
             text = f.read()
@@ -26,18 +28,35 @@ def get_model(corpus):
     return text_model
 
 
+def clean(string):
+    tw = string
+    tw = re.sub("(https?://.*)|(www\..*)|(t\.co.*)|(amzn\.to.*)( |$)|", "", tw)  # remove links + newlines
+    tw = re.sub("\n", "", tw)
+    return tw
+
+
 def get_corpus():
+    # russian trolls, with an ID set for constant time user access
     users = pd.read_csv("users.csv")
-    users.set_index("id")
+    users.set_index(keys="id", inplace=True, drop=False)
     tweets = pd.read_csv("tweets.csv")
 
+    # sort users by their ratio of favorites per follower
     users["favorites_per_follower"] = users.apply(lambda row: row.favourites_count / (row.followers_count + 1), axis=1)
     users.sort_values(by=["favorites_per_follower"], ascending=False, inplace=True)
-    users["tweets"] = np.empty((len(users), 0)).tolist()  # add an empty list to add tweets to
-    for tweet in tweets:
-        user = users.query("id == " + tweet.user_id)
-        user.tweets.append([tweet.text, tweet.favorite_count])
-        print user
+
+    # create a column of empty lists to collect tweets in, then make a list of dictionaries from users
+    users["tweets"] = np.empty((len(users), 0)).tolist()
+    curated_tweets = pd.Series(users.tweets.values, index=users.id).to_dict()
+
+    for index, tweet in tweets.iterrows():
+        if isnan(tweet.user_id) or "RT" in str(tweet.text):  # ignore unidentified tweets and retweets 
+            continue
+        # curate list of tweets and their # of favorites
+        curated_tweets[tweet.user_id].append([str(tweet.text), tweet.favorite_count if not "nan" else 0])
+    # sort the list by # of favorites then combine its contents into a corpus, taking care to clean the text
+    corpus = " ".join(" ".join(clean(z[0]) for z in sorted(x, key=lambda y: y[1])) for x in curated_tweets.values())
+    return corpus
 
 
 if __name__ == "__main__":
